df67d2b27b85d9dd50bc58572e2015bd277e5607
michaellu15 <michaellu15161516@gmail.com>
2025-06-18 03:17:28 -0700

    Added some comments and re added the forgotten history tracking

--- pish.c
+++ pish.c
@@ -34,23 +34,36 @@ void prompt(void) {
         free(working_dir);
     }
 }
-
+/*
+ * Prints out the usage error message
+ */
 void usage_error(void) {
     fprintf(stderr, "pish: Usage error\n");
     fflush(stderr);
 }
+/*
+ * Helper function which trims whitespace from a string
+ * @param string    A string to remove the leading and trailing whitespace
+ * @return          The parsed string without the leading and trailing
+ * whitespace
+ */
 char *trim_whitespace(char *string) {
     char *end;
+    // remove leading space type characters
     while (isspace((unsigned char)*string)) {
         string++;
     }
+    // if we are at the end of a string, then we return the string which is just
+    // a null-terminator
     if (*string == '\0') {
         return string;
     }
+    // remove the trailing space type characters
     end = string + strlen(string) - 1;
     while (end > string && isspace((unsigned char)*end)) {
         end--;
     }
+    // null terminate the string
     *(end + 1) = '\0';
     return string;
 }
@@ -58,14 +71,15 @@ char *trim_whitespace(char *string) {
  * Break down a line of input by whitespace, and put the results into
  * a struct pish_arg to be used by other functions.
  *
- * @param command   A char buffer containing the input command
- * @param arg       Broken down args will be stored here
+ * @param command_str   A char buffer containing the input command
+ * @param arg           Broken down args will be stored here
  */
 void parse_command(char *command_str, struct pish_arg *arg) {
+    // get the token before the first tab character from command_str
     char *saveptr;
     char *token = strtok_r(command_str, " \t", &saveptr);
     arg->argc = 0;
-
+    // remove all tab characters
     while (token != NULL) {
         char *argument = malloc(strlen(token) + 1);
         if (argument == NULL) {
@@ -82,15 +96,18 @@ void parse_command(char *command_str, struct pish_arg *arg) {
 }
 
 /*
- * Run a command.
+ * Run a simple command
  *
  * Built-in commands are handled internally by the pish program.
  * Otherwise, use fork/exec to create child processes to run the program.
  *
- * If the command is empty, do nothing.
- * If NOT in script mode, add the command to history file.
+ * If the command is empty, it does nothing.
+ * If NOT in script mode, adds the command to history file.
+ * @param arg       The arg stored as a struct which contains the arg count and
+ * the char* array of args
  */
 void run(struct pish_arg *arg) {
+    // if the arg count is 0, we simply return
     if (arg->argc == 0) {
         last_exit_status = 0;
         return;
@@ -101,6 +118,7 @@ void run(struct pish_arg *arg) {
         last_exit_status = 1;
         return;
     } else if (pid == 0) {
+        // normal exec behavior
         execvp(arg->argv[0], arg->argv);
         perror(arg->argv[0]);
         exit(127);
@@ -111,6 +129,7 @@ void run(struct pish_arg *arg) {
             last_exit_status = 1;
             return;
         }
+        // store the last exit status
         if (WIFEXITED(status)) {
             last_exit_status = WEXITSTATUS(status);
         } else if (WIFSIGNALED(status)) {
@@ -121,15 +140,23 @@ void run(struct pish_arg *arg) {
     }
 }
 int execute_chain(char *chain);
+/*
+ * This method handles the execution of a subshell
+ * @param command_str    The command to be run in the subshell without the
+ * beginning and trailing parenthesis
+ * @return The exit status of the subshell
+ */
 int run_subshell(char *command_str) {
     pid_t pid = fork();
     if (pid < 0) {
         perror("fork");
         return 1;
     } else if (pid == 0) {
+        // recursively executes the line inside the subshell
         int subshell_status = execute_chain(command_str);
         exit(subshell_status);
     } else {
+        // return the exit status to the parent shell
         int status;
         if (waitpid(pid, &status, 0) == -1) {
             perror("waitpid");
@@ -145,17 +172,29 @@ int run_subshell(char *command_str) {
     }
 }
 static char prevDir[MAX_COMMAND_LENGTH];
+/*
+ * This function is responsible for the execution of a command including the
+ * parsing of separation operators
+ * @param chain      The command to execute as a string
+ * @return           The status of the command which executes
+ */
 int execute_chain(char *chain) {
+    // store the parenthesis level for the subshell
     int paren_level = 0;
     char *scanner = chain;
+    // parse the string for subshells and semi-colon separation
     while (*scanner != '\0') {
         if (*scanner == '(') {
+            // keep track of the level of parenthesis
             paren_level++;
         } else if (*scanner == ')') {
+            // don't want the parenthesis level to be negative
             if (paren_level > 0)
                 paren_level--;
         } else if (*scanner == ';' && paren_level == 0) {
             *scanner = '\0';
+            // recursively execute the left side and the right side of the
+            // semi-colon in the chain
             execute_chain(chain);
             return execute_chain(scanner + 1);
         }
@@ -164,6 +203,7 @@ int execute_chain(char *chain) {
 
     paren_level = 0;
     scanner = chain;
+    // re-parse the string for parenthesis and && and || separation
     while (*scanner != '\0') {
         if (*scanner == '(') {
             paren_level++;
@@ -171,6 +211,8 @@ int execute_chain(char *chain) {
             if (paren_level > 0)
                 paren_level--;
         } else if (paren_level == 0) {
+            // upon reaching a &&, execute the left side then execute the right
+            // side if the left side has an exit status of 0
             if (strncmp(scanner, "&&", 2) == 0) {
                 *scanner = '\0';
                 int status = execute_chain(chain);
@@ -179,7 +221,10 @@ int execute_chain(char *chain) {
                 } else {
                     return status;
                 }
-            } else if (strncmp(scanner, "||", 2) == 0) {
+            }
+            // upon reaching a ||, execute the left side then execute the right
+            // side if the left side has an non-zero exit status
+            else if (strncmp(scanner, "||", 2) == 0) {
                 *scanner = '\0';
                 int status = execute_chain(chain);
                 if (status != 0) {
@@ -191,10 +236,13 @@ int execute_chain(char *chain) {
         }
         scanner++;
     }
-
+    // keep track of the local exit status
     int local_status = 0;
     char *trimmed_cmd = trim_whitespace(chain);
-    if (strlen(trimmed_cmd)>0&&*trimmed_cmd == '!') {
+    // check if the start of the trimmed and parsed command starts with a bang
+    // character
+    if (strlen(trimmed_cmd) > 0 && *trimmed_cmd == '!') {
+        // if it does, handle the command and negate the exit status
         char *post_bang_cmd = trimmed_cmd + 1;
         int status = execute_chain(post_bang_cmd);
         if (status == 0) {
@@ -203,7 +251,10 @@ int execute_chain(char *chain) {
             local_status = 0;
         }
     }
+    // upon reaching a parenthesis opening, we run the contents inside the
+    // parenthesis as a subshell
     else if (*trimmed_cmd == '(') {
+        // check it is a valid subshell syntax
         char *end = trimmed_cmd + strlen(trimmed_cmd) - 1;
         if (*end == ')') {
             *end = '\0';
@@ -213,28 +264,36 @@ int execute_chain(char *chain) {
             fprintf(stderr, "pish: syntax error: missing ')'\n");
             local_status = 2;
         }
-    } else if (strlen(trimmed_cmd) == 0) {
+    }
+    // if the trimmed command is empty, do nothing
+    else if (strlen(trimmed_cmd) == 0) {
         local_status = 0;
-    } else {
+    }
+    // execute the basic command which has been parsed already for all the
+    // special characters
+    else {
         struct pish_arg *cmd = malloc(sizeof(struct pish_arg));
         if (cmd == NULL) {
             perror("malloc");
             exit(EXIT_FAILURE);
         }
         parse_command(trimmed_cmd, cmd);
-
+        // if the command is empty, do nothing
         if (cmd->argv[0] == NULL) {
             local_status = 0;
-        } else if (strcmp(cmd->argv[0], "cd") == 0) {
+        }
+        // if the command is cd, change directory
+        else if (strcmp(cmd->argv[0], "cd") == 0) {
             if (cmd->argc != 2) {
                 usage_error();
                 local_status = 1;
             } else {
                 char *cwd = getcwd(NULL, 0);
+                // handle cd - which changes to the previous directory the shell
+                // was in
                 if (strcmp(cmd->argv[1], "-") == 0) {
                     if (prevDir[0] == '\0') {
-                        fprintf(stderr, "pish: cd: OLDPWD not set\n");
-                        local_status = 1;
+                        printf("%s\n", cwd);
                     } else {
                         char temp[MAX_COMMAND_LENGTH];
                         strcpy(temp, prevDir);
@@ -251,7 +310,9 @@ int execute_chain(char *chain) {
                         }
                     }
                 } else {
+                    // store the old directory
                     strcpy(prevDir, cwd);
+                    // change the directory
                     if (chdir(cmd->argv[1]) == -1) {
                         perror("cd");
                         local_status = 1;
@@ -261,13 +322,19 @@ int execute_chain(char *chain) {
                 }
                 free(cwd);
             }
-        } else if (strcmp(cmd->argv[0], "exit") == 0) {
+        }
+        // if the command executed is exit
+        else if (strcmp(cmd->argv[0], "exit") == 0) {
+            // arg count should be greater than 2
             if (cmd->argc > 2) {
                 usage_error();
                 local_status = 1;
-            } else if (cmd->argc == 2) {
+            }
+            // exit with the desired exit code
+            else if (cmd->argc == 2) {
                 char *endptr;
                 long status_val = strtol(cmd->argv[1], &endptr, 10);
+                // check that the inputted exit code is a valid number
                 if (*endptr != '\0' || endptr == cmd->argv[1]) {
                     fprintf(stderr, "pish: exit: numeric argument required\n");
                     local_status = 2;
@@ -312,17 +379,18 @@ int execute_chain(char *chain) {
 
     return local_status;
 }
-int check_for_continuation(char *line){
+int check_for_continuation(char *line) {
     int len = strlen(line);
-    if (len==0){
+    if (len == 0) {
         return 0;
     }
-    if(line[len-1]=='\\'){
-        line[len-1]='\0';
+    if (line[len - 1] == '\\') {
+        line[len - 1] = '\0';
         return 1;
     }
-    if(len>=2){
-        if(strcmp(&line[len-2],"&&")==0 || strcmp(&line[len-2],"||")==0){
+    if (len >= 2) {
+        if (strcmp(&line[len - 2], "&&") == 0 ||
+            strcmp(&line[len - 2], "||") == 0) {
             return 2;
         }
     }
@@ -351,7 +419,8 @@ int pish(FILE *fp) {
                 if (full_command) {
                     last_exit_status = execute_chain(full_command);
                 }
-                if (!script_mode && isatty(fileno(stdin))) printf("\n");
+                if (!script_mode && isatty(fileno(stdin)))
+                    printf("\n");
                 return last_exit_status;
             }
 
@@ -367,23 +436,23 @@ int pish(FILE *fp) {
                 }
             } else {
                 const char *separator;
-                if(continuation_type==1){
+                if (continuation_type == 1) {
                     separator = "";
-                }
-                else{
+                } else {
                     separator = " ";
                 }
                 size_t old_len = strlen(full_command);
                 size_t sep_len = strlen(separator);
                 size_t new_part_len = strlen(trimmed_line);
 
-                char* new_full_command = realloc(full_command, old_len + sep_len + new_part_len + 1);
+                char *new_full_command =
+                    realloc(full_command, old_len + sep_len + new_part_len + 1);
                 if (new_full_command == NULL) {
                     perror("realloc failed");
                     exit(EXIT_FAILURE);
                 }
                 full_command = new_full_command;
-                
+
                 strcat(full_command, separator);
                 strcat(full_command, trimmed_line);
             }
@@ -395,8 +464,24 @@ int pish(FILE *fp) {
                 fflush(stdout);
             }
         } while (continuation_type != 0);
-        
+
         if (full_command && strlen(full_command) > 0) {
+            if(!script_mode){
+                char *history_copy = strdup(full_command);
+                if(history_copy==NULL){
+                    perror("strdup failed for history");
+                    exit(EXIT_FAILURE);
+                }
+                struct pish_arg history_arg;
+                parse_command(history_copy, &history_arg);
+                if(history_arg.argc>0){
+                    add_history(&history_arg);
+                }
+                for(int i = 0; i<history_arg.argc;i++){
+                    free(history_arg.argv[i]);
+                }
+                free(history_copy);
+            }
             char *command_copy = strdup(full_command);
             if (command_copy == NULL) {
                 perror("strdup failed");
@@ -405,7 +490,7 @@ int pish(FILE *fp) {
             last_exit_status = execute_chain(command_copy);
             free(command_copy);
         } else {
-             last_exit_status = 0;
+            last_exit_status = 0;
         }
 
         if (full_command) {
